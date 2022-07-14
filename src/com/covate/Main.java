d74c6565ad10bb0d34b9dd73d10dc19cef660cd4
Elisée MULENGA <mulengaelisee2@gmail.com>
2022-07-14 09:58:09 +0200

    Ajout du fichier main

--- src/com/covate/Main.java
+++ src/com/covate/Main.java
@@ -1,4 +1,12 @@
 package com.covate;
 
 public class Main {
+    public static void main(String[] args) {
+        Write e1 = new Write("bonjour", 10,5);
+        Write e2 = new Write("Bonsoir", 12,14);
+        Write e3 = new Write("\n", 6,18);
+        e1.start();
+        e2.start();
+        e3.start();
+    }
 }
